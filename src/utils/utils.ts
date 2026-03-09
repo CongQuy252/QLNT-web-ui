2088f1a4ac2dd4f401349e0337bc24cdb84d8b87
quyok808 <quyok25203@gmail.com>
2026-03-09 21:45:40 +0700

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -12,3 +12,20 @@ export const formatDate = (isoString: string): string => {
   const date = new Date(isoString);
   return date.toLocaleDateString('vi-VN');
 };
+
+// Chuyển số thành chuỗi có phân cách (1000 -> 1,000)
+export const formatNumber = (value: string | number) => {
+  if (!value) return '';
+  const number = value.toString().replace(/\D/g, '');
+  return number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+// Chuyển chuỗi định dạng về số (1,000 -> 1000)
+export const parseNumber = (value: string): number | undefined => {
+  const cleaned = value.replace(/,/g, '');
+
+  if (!cleaned) return undefined;
+
+  const num = Number(cleaned);
+  return Number.isNaN(num) ? undefined : num;
+};
